48fb74feb88c3c02e0e6b3aeac2e529b5a5352aa
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2020-04-03 21:07:53 +0900

    Show back stack log

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.navigationsample
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        backStackLogButton.setOnClickListener {
+            backStackLogTextView.text = findNavController(R.id.navHostFragment)
+                .backStack
+                .map { it.toString() }
+                .reduce { acc, navBackStackEntryString ->
+                    acc + navBackStackEntryString + "\n"
+                }
+        }
     }
 }
